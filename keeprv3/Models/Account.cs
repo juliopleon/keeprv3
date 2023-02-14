19b8c7e1d763af8ff4ccb06132d45845951381e9
Julio Leon <juliopena143@icloud.com>
2023-02-13 21:53:00 -0700

    restart

---
+++ keeprv3/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace keeprv3.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
