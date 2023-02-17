3985d4a1d95022b4deb556911cf8344115b2b20a
Julio Leon <juliopena143@icloud.com>
2023-02-16 20:48:58 -0700

    sql database setup

--- keeprv3/Models/VaultKeep.cs
+++ keeprv3/Models/VaultKeep.cs
@@ -1,1 +1,10 @@
 namespace keeprv3.Models;
+
+public class VaultKeep
+{
+    public int Id { get; set; }
+    public string CreatorId { get; set; }
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
+    public Account Creator { get; set; }
+}
--- keeprv3/dbSetup.sql
+++ keeprv3/dbSetup.sql
@@ -1,8 +1,13 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+ALTER TABLE accounts
+ADD
+    COLUMN coverImg VARCHAR(255) NOT NULL DEFAULT "https://media.istockphoto.com/id/1141304089/vector/global-network-connection-concept-big-data-visualization-social-network-communication-in-the.jpg?s=612x612&w=0&k=20&c=XoQRixTQExBgu3CKeSzTleQSbasbpBkDs3S3q6BByOM=";
