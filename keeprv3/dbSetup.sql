834faf3db2f3c98b674d22e68e95833cb4e09ebf
Julio Leon <juliopena143@icloud.com>
2023-02-20 21:10:52 -0700

    sql connection problems

--- keeprv3/dbSetup.sql
+++ keeprv3/dbSetup.sql
@@ -11,3 +11,15 @@ CREATE TABLE
 ALTER TABLE accounts
 ADD
     COLUMN coverImg VARCHAR(255) NOT NULL DEFAULT "https://media.istockphoto.com/id/1141304089/vector/global-network-connection-concept-big-data-visualization-social-network-communication-in-the.jpg?s=612x612&w=0&k=20&c=XoQRixTQExBgu3CKeSzTleQSbasbpBkDs3S3q6BByOM=";
+
+CREATE TABLE
+    IF NOT EXISTS keeps(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        creatorId VARCHAR(250) NOT NULL,
+        name VARCHAR(250) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        img VARCHAR(255) NOT NULL DEFAULT "https://images.ctfassets.net/b4k16c7lw5ut/TdKLskzZYA5IJddHkSvQE/23850e128cb6c060851b2371ccfabef3/Clipchamp-Pinterest-partnership-step-3.jpg?w=1000&h=563&fl=progressive&q=50&fm=jpg",
+        views INT NOT NULL DEFAULT 0,
+        kept INT NOT NULL DEFAULT 0,
+        FOREIGN KEY(creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
