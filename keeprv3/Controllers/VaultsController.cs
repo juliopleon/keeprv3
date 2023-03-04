027997a58b99b90c340aa43358eca584585a5acc
Julio Leon <juliopena143@icloud.com>
2023-03-03 21:07:10 -0700

    create

--- keeprv3/Controllers/VaultsController.cs
+++ keeprv3/Controllers/VaultsController.cs
@@ -5,5 +5,33 @@ namespace keeprv3.Controllers;
 [Route("api/[controller]")]
 public class VaultsController : ControllerBase
 {
+    private readonly VaultsService _vaultsService;
+    private readonly Auth0Provider _auth0Provider;
 
+    public VaultsController(VaultsService vaultsService, Auth0Provider auth0Provider)
+    {
+        _vaultsService = vaultsService;
+        _auth0Provider = auth0Provider;
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<Vault>> Create([FromBody] Vault vaultData)
+    {
+        try
+        {
+            Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+            vaultData.CreatorId = userInfo.Id;
+
+            Vault vault = _vaultsService.Create(vaultData);
+            vault.Creator = userInfo;
+
+            return Ok(vault);
+
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
 }
--- keeprv3/Services/VaultsService.cs
+++ keeprv3/Services/VaultsService.cs
@@ -2,5 +2,15 @@ namespace keeprv3.Services;
 
 public class VaultsService
 {
+    private readonly VaultsRepository _repo;
 
+    public VaultsService(VaultsRepository repo)
+    {
+        _repo = repo;
+    }
+
+    internal Vault Create(Vault vaultData)
+    {
+
+    }
 }
