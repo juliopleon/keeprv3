10ba36af65ac5ba3bb73490747e616a783450e86
Julio Leon <juliopena143@icloud.com>
2023-03-09 11:31:20 -0700

    ye

--- keeprv3/Services/VaultsService.cs
+++ keeprv3/Services/VaultsService.cs
@@ -30,7 +30,7 @@ public class VaultsService
         return vault;
     }
 
-    internal Vault Updat(Vault vaultUpdate)
+    internal Vault Update(Vault vaultUpdate)
     {
         Vault original = GetOneVault()
     }
