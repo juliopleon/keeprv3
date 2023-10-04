241b14b85fa5b0aa81c528c3c0aa55ec6ee10528
Julio Leon <juliopena143@icloud.com>
2023-10-04 00:08:11 -0600

    router

--- keeprv3.client/src/router.js
+++ keeprv3.client/src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import { authGuard } from '@bcwdev/auth0provider-client'
+import { authGuard, authSettled } from '@bcwdev/auth0provider-client'
 
 function loadPage(page) {
   return () => import(`./pages/${page}.vue`)
@@ -21,6 +21,19 @@ const routes = [
     name: 'Account',
     component: loadPage('AccountPage'),
     beforeEnter: authGuard
+  },
+  {
+    path: '/profiles/:id',
+    name: 'Profile',
+    component: loadPage('ProfilesPage'),
+    beforeEnter: authSettled
+  },
+
+  {
+    path: '/vaults/:id',
+    name: 'Vault',
+    component: loadPage('VaultsPage'),
+    beforeEnter: authSettled
   }
 ]
 
