cc4178d148fdc89435fa5da30651857301cc9b7b
Julio Leon <juliopena143@icloud.com>
2023-10-03 22:42:01 -0600

    bug fixed

--- keeprv3/Models/Account.cs
+++ keeprv3/Models/Account.cs
@@ -6,4 +6,5 @@ public class Account
   public string Name { get; set; }
   public string Email { get; set; }
   public string Picture { get; set; }
+  public object CoverImg { get; internal set; }
 }
